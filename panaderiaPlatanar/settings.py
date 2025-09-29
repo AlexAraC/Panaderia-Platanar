b58c253f4e0667dac83a18fba93f9403aa252706
AlexAraC <arayaalex886@gmail.com>
2025-09-29 09:14:08 -0600

    Fix ALLOWED_HOSTS for Render

--- panaderiaPlatanar/settings.py
+++ panaderiaPlatanar/settings.py
@@ -23,12 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "insecure-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ALLOWED_HOSTS = ["*", "panaderia-platanar-san-carlos.onrender.com", "localhost", "127.0.0.1"]
 
-ALLOWED_HOSTS = [
-    "panaderia-platanar-san-carlos.onrender.com",
-    "localhost",
-    "127.0.0.1",
-]
 
 # Application definition
 
